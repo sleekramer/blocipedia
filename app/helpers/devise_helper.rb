f608821229a193a4991f43a1cf921ee41fef8e1a
Steven Lee-Kramer <sleekramer@gmail.com>
2015-11-10 18:22:55 -0500

    Correct error in app/helpers/devise_helper.rb

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,8 +1,8 @@
 module DeviseHelper
   def devise_error_messages!
-    return '' if resources.errors.empty?
+    return '' if resource.errors.empty?
 
-    messages = resources.errors.full_messages.map {|msg| content_tag(:li, msg)}.join
+    messages = resource.errors.full_messages.map {|msg| content_tag(:li, msg)}.join
     html = <<-HTML
     <div data-alert class="alert-box alert">
       #{messages}
