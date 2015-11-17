4f8529f7d372b70b8da3cc515ff092edf00ae8bf
Steven Lee-Kramer <sleekramer@gmail.com>
2015-11-17 13:42:47 -0500

    complete user story for private wikis.

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,11 @@
 module WikisHelper
+  def display_wiki_content?(wiki)
+    (wiki.private && (wiki.user == current_user || current_user.admin?)) || !wiki.private
+  end
+  def user_authorized_for_private?(wiki)
+    ((wiki.user == current_user || !wiki.user) && current_user.premium?) || current_user.admin?
+  end
+  def display_in_index?(wiki)
+    wiki.user != current_user && display_wiki_content?(wiki)
+  end
 end
