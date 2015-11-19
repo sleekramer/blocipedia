d76a85ffbe404f7360b4f75925c54bf5c7fa3dda
Steven Lee-Kramer <sleekramer@gmail.com>
2015-11-19 15:11:06 -0500

    Accept user emails in collaborator form

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -2,11 +2,16 @@ class Collaborator < ActiveRecord::Base
   belongs_to :user
   belongs_to :wiki
 
+  delegate :email, to: :user, prefix: true
+
   def self.update_collaborators(wiki, collaborators_string)
     return [] if collaborators_string.blank?
+    new_collabos = collaborators_string.split(",").map(&:strip)
+    wiki.collaborators.each {|c| c.destroy unless new_collabos.include?(c.user_email)}
+    user_ids = User.where(email: new_collabos).pluck(:id)
+    user_ids.map do |user_id|
 
-    collaborators_string.split(",").map do |collaborator|
-      Collaborator.find_or_create_by(wiki_id: wiki.id, user_id: collaborator)
+      Collaborator.create(wiki_id: wiki.id, user_id: user_id)
     end
   end
 end
