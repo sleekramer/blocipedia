c4f9c8eab03003a941c7de0541466bafdb7fdd54
Charlie Gaines <beaugaines@gmail.com>
2015-11-19 09:25:26 -0500

    rename has_many through association to avoid conflict with existing has_many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :wikis
   has_many :collaborators
-  has_many :wikis, through: :collaborators
+  has_many :shared_wikis, through: :collaborators, source: :wiki
 
   after_initialize :set_default_role
 
